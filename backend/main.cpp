e2e625e09f446d406d6080c7ecd0b4aec4bfbe3d
Ishan Sandhum <ishan.sandhu3121@gmail.com>
2026-01-20 22:30:17 -0800

    figuring out sockets and adding some personal notes as comments

--- backend/main.cpp
+++ backend/main.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 using boost::asio::ip::tcp;
 
-// void hello(){
-//     cout<<"hello connected"<<endl;
-// }
 int main(){
 
+    // using epoll under the hood for event loops and stuff
     boost::asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(),8080));
+    // creating a v4 endpoint with port 8080 for the main server
+    tcp::endpoint endpoint(tcp::v4(),8080);
+    // listening socket (accepting connections)
+    tcp::acceptor acceptor(io_context, endpoint);
 
     for(;;){
+        // creating an empty socket object
         tcp::socket sock(io_context);
+        // accepting the connection (tcp 3way handshake) and adding that to the empty socket
         acceptor.accept(sock);
         cout<<"connected successfully"<<endl;
-        // boost::asio::write(socket)
+        cout<<"connection closed"<<endl;
     }
     return 0;
 }
