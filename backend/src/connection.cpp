7b7afe95c138773f0dcb7031be1afdf12658767f
xIshanSandhux <ishan.sandhu3121@gmail.com>
2026-07-15 19:09:57 -0700

    making the http server

--- backend/src/connection.cpp
+++ backend/src/connection.cpp
@@ -14,11 +14,18 @@ tcp::socket& TcpConnection::socket(){
 
 // processing the request
 void TcpConnection::process(){
+std::string response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 16\r\n"
+        "\r\n"
+        "sample response\n";
+cout<<"ENETERED THE PROCESS FUNCTION"<<endl;
     // gives the OS what to send as a response and because this is async need a callback
     // to make sure everything was sent as expected without errors
     // the callback gets triggered once the io detects that the message was sent
     // as expected or got an error back
-    boost::asio::async_write(socket_,boost::asio::buffer("sample response"),
+    boost::asio::async_write(socket_,boost::asio::buffer(response),
             [self=shared_from_this()] (const boost::system::error_code& ec, std::size_t bt){
             self->handle_write(ec,bt);
             }
@@ -29,6 +36,7 @@ void TcpConnection::process(){
 void TcpConnection::handle_write(const boost::system::error_code& ec, std::size_t bt){
     if (ec){
         cout<<"ERROR OCCURED WHILE WRITING BACK TO THE CLIENT"<<endl;
+        return;
     }
     cout<<"SUCCESSFULLY COMPLETED THE REQUEST. WRITTEN: "<<bt<<" BYTES" <<endl;
 }
